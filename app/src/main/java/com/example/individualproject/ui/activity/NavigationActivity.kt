d4e372aaa99c8857c8ad9d8fba3e2e6d77471ec6
garima shrestha <garimashrestha20@gmail.com>
2025-03-01 09:21:54 +0545

    Bottom Navigation

--- app/src/main/java/com/example/individualproject/ui/activity/NavigationActivity.kt
+++ app/src/main/java/com/example/individualproject/ui/activity/NavigationActivity.kt
@@ -1,20 +1,75 @@
-package com.example.individualproject.ui.activity
+//package com.example.individualproject.ui.activity
+//
+//import android.os.Bundle
+//import androidx.activity.enableEdgeToEdge
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+//import com.example.individualproject.R
+//
+//class NavigationActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_navigation)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
+
+
+
+package com.example.byayamrecords.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.individualproject.R
+import com.example.individualproject.databinding.ActivityNavigationBinding
+import com.example.individualproject.ui.fragment.ExpenseCalculatorFragment
+import com.example.individualproject.ui.fragment.ExpenseLogFragment
+import com.example.individualproject.ui.fragment.ProfileFragment
+
 
 class NavigationActivity : AppCompatActivity() {
+    lateinit var navigationBinding: ActivityNavigationBinding
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_navigation)
+        navigationBinding = ActivityNavigationBinding.inflate(layoutInflater)
+
+        setContentView(navigationBinding.root)
+
+        replaceFragment(ExpenseLogFragment())
+        navigationBinding.buttomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_expense_calculator -> replaceFragment(ExpenseCalculatorFragment())
+                R.id.nav_expense_log -> replaceFragment(ExpenseLogFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+                else -> {}
+            }
+            true
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
     }
